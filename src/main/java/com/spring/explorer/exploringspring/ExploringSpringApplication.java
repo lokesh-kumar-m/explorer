29b43bf830c3746acc27fcd0741c6346a80fd42d
lokeshmekala <mekalalokesh1999@gmail.com>
2024-03-07 10:06:32 +0100

    initialize

---
+++ src/main/java/com/spring/explorer/exploringspring/ExploringSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.explorer.exploringspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploringSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploringSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/explorer/exploringspring/ExploringSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.explorer.exploringspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploringSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
