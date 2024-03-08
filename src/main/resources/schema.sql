112be552c1a4b709d6d756a60911c846ee6c4ed8
lokeshmekala <mekalalokesh1999@gmail.com>
2024-03-08 20:38:03 +0100

    create table in h2

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table course(
+    id number not null,
+    name varchar(225) not null,
+    author varchar(225) not null,
+    primary key (id)
+);
--- src/test/java/com/dev/explorejpahibernate/ExploreJpaHibernateApplicationTests.java
+++ src/test/java/com/dev/explorejpahibernate/ExploreJpaHibernateApplicationTests.java
@@ -1,10 +1,10 @@
-package com.spring.explorer.exploringspring;
+package com.dev.explorejpahibernate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ExploringSpringApplicationTests {
+class ExploreJpaHibernateApplicationTests {
 
 	@Test
 	void contextLoads() {
