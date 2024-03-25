0570cfe5f43cf760b2afaf4eb64ef8926e68ca10
lokeshmekala <mekalalokesh1999@gmail.com>
2024-03-25 14:56:41 +0100

    adding unit testing

---
+++ src/main/java/com/dev/explorer/ExploringUnitTesting.java
@@ -0,0 +1,12 @@
+package com.dev.explorer;
+public class ExploringUnitTesting {
+    
+    public int sumOfNumbers(int number){
+        int sum=0;
+        for(int i=1;i<=number;i++){
+            sum+=i;
+        }
+        return sum;
+    }
+
+}
